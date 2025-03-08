ced4ff201e8c6406f4b972be591c0f5045bf197f
Arthur Cabon <djarth2222@gmail.com>
2025-03-07 20:15:58 -0500

    feat: config between streamrip and cabot

--- src/features/config.py
+++ src/features/config.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 import json
+import toml
+from streamrip.config import DEFAULT_CONFIG_PATH
 
 # region Utils
 def get_project_root() -> Path :
@@ -16,28 +18,85 @@ def get_project_root() -> Path :
 
 CABOT = get_project_root()
 CONFIG_PATH = CABOT / "config.json"
+CONFIG_CORRESPONDANCE = {
+    ("qobuz", "email"): ("qobuz", "email_or_userid"),
+    ("qobuz", "token"): ("qobuz", "password_or_token"),
+    ("tmp_folder",): ("downloads", "folder")
+}
 
 # endregion
 
-def get_config_value(key: str) -> str :
+
+# region Cabot
+def get_cabot_config_value(keys: list[str]) -> str :
 
     with open(CONFIG_PATH, 'r') as f :
         config_data = json.load(f)
     
-    assert key in config_data, f"{key} n'est pas configuré : cabot config -{key} 'value'"
+    tmp_dict_or_value = config_data
+    for key in keys :
+        assert isinstance(tmp_dict_or_value, dict)
+        assert key in tmp_dict_or_value
+        tmp_dict_or_value = tmp_dict_or_value[key]
 
-    return config_data[key]
+    return tmp_dict_or_value
 
 
-def set_config_value(key: str, value: str) -> None :
+def set_cabot_config_value(keys: list[str], value: str) -> None :
 
     with open(CONFIG_PATH, 'r') as f :
         config_data = json.load(f)
     
-    config_data[key] = value
+    tmp_dict = config_data
+
+    for key in keys[:-1] :
+        if key not in tmp_dict :
+            tmp_dict[key] = {}
+        tmp_dict = tmp_dict[key]
     
+    tmp_dict[keys[-1]] = value
+
     with open(CONFIG_PATH, 'w') as f :
         json.dump(config_data, f, indent=4)
     
     return
+
+# endregion
+
+# region streamrip
+def set_streamrip_config_value(region: str, key: str, value: str) -> None :
+
+    config_data = toml.load(DEFAULT_CONFIG_PATH)
+    
+    if region not in config_data :
+        config_data[region] = {}
+    config_data[region][key] = value
+    
+    with open(DEFAULT_CONFIG_PATH, 'w') as f :
+        toml.dump(config_data, f)
+    
+    return
+
+
+def apply_cabot_config_to_streamrip() -> None :
+
+    for cabot_keys, (rip_region, rip_key) in CONFIG_CORRESPONDANCE.items() :
+        value = get_cabot_config_value(cabot_keys)
+        set_streamrip_config_value(rip_region, rip_key, value)
+
+    return
+
+# endregion 
+
+
+# region default
+def initialize_config() -> None :
+    apply_cabot_config_to_streamrip()
+    set_streamrip_config_value("qobuz", "use_auth_token", "true")
+
+def set_default_config() -> None :
+    set_cabot_config_value(["tmp_folder"], str(CABOT / "tmp_download"))
+    initialize_config()
+
+# endregion
 
