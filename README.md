e61d943991a12a388f721b73e4910fe7671ccadc
Arthur Cabon <57801065+ArthurCbn@users.noreply.github.com>
2025-03-10 20:17:57 -0400

    feat: add installation script (only for linux) (#7)

--- README.md
+++ README.md
@@ -10,4 +10,15 @@ ffmpeg
 
 ```
 git clone https://ArthurCbn:**key**@github.com/ArthurCbn/cabot.git
+cd cabot
+bash installation.sh
+source ~/.bashrc
+```
+
+## Usage
+
+One command for one feature : updating all your playlists (download in lossless, convert, analyse key) 
+
+```
+cabot
 ```
---
+++ install.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+# python requirements
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+# Fixing streamrip qobuz
+QOBUZ_PATH=$(python -c "import streamrip.client.qobuz as qb ; print(qb.__file__)")
+cp -f src/streamrip/qobuz.py $QOBUZ_PATH
+
+# Add the alias to .bashrc (for bash shell users)
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+CABOT_COMMAND="CWD=$(pwd) ; cd $SCRIPT_DIR && python -m src.main && cd \$CWD"
+
+sed -i '/cabot/d' ~/.bashrc
+echo "alias cabot='$CABOT_COMMAND'" >> ~/.bashrc
