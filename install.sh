66bd4a713626c2ad66f7758363bde9cf706a896a
Arthur Cabon <djarth2222@gmail.com>
2025-03-22 14:58:32 -0400

    feat: stop tweaking streamrip

--- install.sh
+++ install.sh
@@ -7,14 +7,6 @@ pip install -r requirements.txt
 # Initialize streamrip config file
 rip config path 
 
-# Fixing streamrip qobuz
-QOBUZ_PATH=$(python -c "import streamrip.client.qobuz as qb ; print(qb.__file__)")
-cp -f src/streamrip/qobuz.py $QOBUZ_PATH
-
-# Modif of metadata track
-META_TRACK_PATH=$(python -c "import streamrip.metadata.track as track ; print(track.__file__)")
-cp -f src/streamrip/metadata_track.py $META_TRACK_PATH
-
 # Initialize config
 python -c "from src.features.config import set_default_config ; set_default_config()"
 
---
+++ update.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Pull
+git reset --hard origin/main
+
+# python requirements
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+# Initialize streamrip config file
+rip config path 
